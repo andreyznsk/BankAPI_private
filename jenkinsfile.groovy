7da0aa3cbfea322a3c8c07a1bacce060b4ad7f68
andreyz <andreyznsk@gmail.com>
2022-02-16 10:33:55 +0700

    add build script

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -7,12 +7,12 @@ def OSS_FLAG_STATUS
 def mvnHome
 
 String NEXUS_VERSION
-String project_git_url = 'git@github.com:andreyznsk/BankAPI_private.git'
+String NEXUS_ARTIFACT = 'BankAPI_0001'
+String project_git_url_ssh = 'git@github.com:andreyznsk/BankAPI_private.git'
+String project_git_url_https = 'https://github.com/andreyznsk/BankAPI_private.git'
 String JenkinsCredentialsId = 'ubnt'
 
-String branch = GitBranch
-echo "GitBranch: ${GitBranch}, branch: ${branch}"
-
+String branch = GitBranch.split('/')[-1]
 node('ubuntu') {
 
 
@@ -22,14 +22,14 @@ node('ubuntu') {
 
     executeStage('Checkout in Linux', branch, stageResult) { // Чекаут
         scmVars = checkout(
-                [$class       : 'GitSCM',
+                [$class                           : 'GitSCM',
                  branches                         : [[name: "${GitBranch}"]],
-                 browser                          : [$class: 'GitWeb', repoUrl: 'https://github.com/andreyznsk/BankAPI_private.git'],
+                 browser                          : [$class: 'GitWeb', repoUrl: project_git_url_https],
                  doGenerateSubmoduleConfigurations: false,
                  extensions                       : [[$class: 'CleanCheckout'], [$class: 'LocalBranch', localBranch: branch]],
                  gitTool                          : 'Default',
                  submoduleCfg                     : [],
-                 userRemoteConfigs                : [[credentialsId: JenkinsCredentialsId, url: project_git_url]]
+                 userRemoteConfigs                : [[credentialsId: JenkinsCredentialsId, url: project_git_url_ssh]]
                 ])
     }
 
@@ -38,37 +38,45 @@ node('ubuntu') {
         // Находим NEXUS_VERSION
         String versionPom = sh script: "'${mvnHome}/bin/mvn' help:evaluate -Dexpression=project.version -q -DforceStdout", returnStdout: true
         echo "versionPom: ${versionPom}"
-        String releaseVer = versionPom.replaceAll('-SNAPSHOT', '')
-        NEXUS_VERSION = releaseVer
+        String releaseVersion = versionPom.replaceAll('-SNAPSHOT', '')
+        NEXUS_VERSION = releaseVersion
         echo "NEXUS_VERSION: ${NEXUS_VERSION}"
     }
 
     executeStage('Build Distrib', branch, stageResult) {
-        // Переходим на выбранную ветку
-        sh "pwd"
-        sh "git checkout ${GitBranch}"
-        sh "whoami"
         sh "'${mvnHome}/bin/mvn' --version"
-        sh "'${mvnHome}/bin/mvn' clean install"
-        dir ('BankAPIMain/') {
+        String nextVersion = String.format('%03d', Integer.parseInt(NEXUS_VERSION.split('.')[1]) + 1)
+        echo "next build number: ${nextVersion}"
+        sh "'${mvnHome}/bin/mvn' release:clean release:prepare -DdevelopmentVersion=${nextVersion} -DreleaseVersion=${NEXUS_VERSION} -Dtag=kka-${NEXUS_VERSION} -e"
+        dir('BankAPIMain/') {
             sh "zip -r database.zip . -i database/*.sql"
         }
+        sh "zip -r bankAPI.zip database.zip target/BankAPI.jar"
         archiveArtifacts 'BankAPIMain/database.zip'
         archiveArtifacts 'BankAPIMain/target/BankAPI.jar'
     }
 
-    executeStage('Docker build', branch, stageResult) {
+    executeStage('upload to nexus', branch, stageResult) {
         // Загрузка билда в нексус
-        echo "Deploy NEXUS_VERSION: ${NEXUS_VERSION}"
-        sh 'docker build . -t bankapi'
-        /*println("kkaDistribFile name: ${kkaDistribFile.name}, path: ${kkaDistribFile.path}, " +
-                "directory: ${kkaDistribFile.directory}, length: ${kkaDistribFile.length}")
-        echo "Deploy NEXUS_VERSION: ${NEXUS_VERSION}"
-        sh "mvn deploy:deploy-file -DgeneratePom=true -DartifactId=${NEXUS_ARTIFACT} -Dversion=${NEXUS_VERSION}" +
-                "-Dpackaging=zip -Dfile=${kkaDistribFile.path} -Durl=https:nexus:8081 -Dclassifier=distrib"*/
+        withCredentials([usernamePassword(credentialsId: "nexusUser", usernameVariable: "nexusUser", passwordVariable: "nexusPwd")]) {
+            def bankAipFile = findfiles(glob: 'BankAPIMain/bankAPI.zip')[0]
+            echo "Deploy NEXUS_VERSION: ${NEXUS_VERSION}"
+            echo "bankAipFile {name: ${bankAipFile.name}, path: ${bankAipFile.path}, dir: ${bankAipFile.directory}"
+            sh "'${mvnHome}/bin/mvn' deploy:deploy-file -DgroupId=Nexus_PROD -DgeneratePom=true -DartifactId=${NEXUS_ARTIFACT} -Dversion=${NEXUS_VERSION}" +
+                    "-Dpackaging=zip -Dfile=${bankAipFile.path} -DrepositoryId=sbrf-repo-releases -Durl=http://localhost:8081/" +
+                    "-Drepo.username=${nexusUser} -Drepo.password=${nexusPwd} -Dclassifier=distrib"
+        }
     }
 
+
+//    executeStage('Docker build', branch, stageResult) {
+//        // Загрузка сборка Докер Образ
+//        sh 'docker build . -t bankapi'
+//     }
+
 }
+echo "GitBranch: ${GitBranch}, branch: ${branch}"
+
 
 def executeStage(stageName, branch, stageResult, Closure task) {
     try {

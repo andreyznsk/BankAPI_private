8790705af018746ebd99c3fe80790d19f462e641
andreyz <andreyznsk@gmail.com>
2022-02-16 16:53:53 +0700

    add build script

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -13,6 +13,8 @@ String project_git_url_https = 'https://github.com/andreyznsk/BankAPI_private.gi
 String JenkinsCredentialsId = 'ubnt'
 String nexusReleasesURL = 'http://localhost:8081/repository/maven-releases/'
 String nexusRepoId = 'maven-releases'
+String mavenVersion = '3.5.0'
+String configXml = 'MySettings'
 
 String branch = GitBranch.split('/')[-1]
 node('ubuntu') {
@@ -37,58 +39,57 @@ node('ubuntu') {
         echo "scmVars: ${scmVars}"
         echo "GitBranch: ${GitBranch}, branch: ${branch}"
     }
+    withMaven(maven: mavenVersion, mavenSettingsConfig: configXml) {
+        executeStage('Determine NEXUS_VERSION', branch, stageResult) {
+            //mvnHome = tool '3.5.0'
+            // Находим NEXUS_VERSION
+            String versionPom = sh script: "mvn help:evaluate -Dexpression=project.version -q -DforceStdout", returnStdout: true
+            echo "versionPom: ${versionPom}"
+            String releaseVersion = versionPom.replaceAll('-SNAPSHOT', '')
+            NEXUS_VERSION = releaseVersion
+            echo "NEXUS_VERSION: ${NEXUS_VERSION}"
+        }
 
-    executeStage('Determine NEXUS_VERSION', branch, stageResult) {
-        mvnHome = tool '3.5.0'
-        // Находим NEXUS_VERSION
-        String versionPom = sh script: "'${mvnHome}/bin/mvn' help:evaluate -Dexpression=project.version -q -DforceStdout", returnStdout: true
-        echo "versionPom: ${versionPom}"
-        String releaseVersion = versionPom.replaceAll('-SNAPSHOT', '')
-        NEXUS_VERSION = releaseVersion
-        echo "NEXUS_VERSION: ${NEXUS_VERSION}"
-    }
+        executeStage('Build Distrib', branch, stageResult) {
+            sshagent([JenkinsCredentialsId]) {
 
-    executeStage('Build Distrib', branch, stageResult) {
-        sshagent([JenkinsCredentialsId]) {
-
-            sh 'git config --global user.email "you@example.com"'
-            sh 'git config --global user.name "jenkins"'
-            sh "'${mvnHome}/bin/mvn' --version"
-            String nextVersion = String.format('%03d', Integer.parseInt(NEXUS_VERSION.split('\\.')[1]) + 1)
-            echo "next build number: ${nextVersion}"
-            sh "'${mvnHome}/bin/mvn' release:clean release:prepare " +
-                    "--batch-mode -DautoVersionSubmodules=true -DdevelopmentVersion=${nextVersion} " +
-                    "-DreleaseVersion=${NEXUS_VERSION} -Dtag=BankApi-${NEXUS_VERSION} -e"
-            dir('BankAPIMain/') {
-                sh "zip -r database.zip . -i database/*.sql"
-                sh "zip -r bankAPI.zip database.zip target/BankAPI.jar"
+                sh 'git config --global user.email "you@example.com"'
+                sh 'git config --global user.name "jenkins"'
+                sh "mvn --version"
+                String nextVersion = String.format('%03d', Integer.parseInt(NEXUS_VERSION.split('\\.')[1]) + 1)
+                echo "next build number: ${nextVersion}"
+                sh "mvn release:clean release:prepare " +
+                        " --batch-mode -DautoVersionSubmodules=true -DdevelopmentVersion=${nextVersion} " +
+                        " -DreleaseVersion=${NEXUS_VERSION} -Dtag=BankApi-${NEXUS_VERSION} -e"
+                dir('BankAPIMain/') {
+                    sh "zip -r database.zip . -i database/*.sql"
+                    sh "zip -r bankAPI.zip database.zip target/BankAPI.jar"
+                }
+                archiveArtifacts 'BankAPIMain/database.zip'
+                archiveArtifacts 'BankAPIMain/target/BankAPI.jar'
             }
-            archiveArtifacts 'BankAPIMain/database.zip'
-            archiveArtifacts 'BankAPIMain/target/BankAPI.jar'
         }
-    }
 
-    executeStage('upload to NEXUS', branch, stageResult) {
-        // Загрузка билда в нексус
-        withCredentials([usernamePassword(credentialsId: "admin", usernameVariable: "nexusUser", passwordVariable: "nexusPwd")]) {
-            def bankAipFile = findFiles(glob: 'BankAPIMain/bankAPI.zip')[0]
-            echo "Deploy NEXUS_VERSION: ${NEXUS_VERSION}"
-            echo "bankAipFile {name: ${bankAipFile.name}, path: ${bankAipFile.path}, dir: ${bankAipFile.directory}"
-            echo "-Drepo.username=${nexusUser}"
-            sh "'${mvnHome}/bin/mvn' deploy:deploy-file -DgeneratePom=true -DartifactId=${NEXUS_ARTIFACT} -Dversion=${NEXUS_VERSION}" +
-                    " -Dpackaging=zip -Dfile=${bankAipFile.path} -Durl=${nexusReleasesURL} -DgroupId=maven-public" +
-                    " -Drepo.username=${nexusUser} -Drepo.password=${nexusPwd} -Dclassifier=distrib -e"
+        executeStage('upload to NEXUS', branch, stageResult) {
+            // Загрузка билда в нексус
+            withCredentials([usernamePassword(credentialsId: "admin", usernameVariable: "nexusUser", passwordVariable: "nexusPwd")]) {
+                def bankAipFile = findFiles(glob: 'BankAPIMain/bankAPI.zip')[0]
+                echo "Deploy NEXUS_VERSION: ${NEXUS_VERSION}"
+                echo "bankAipFile {name: ${bankAipFile.name}, path: ${bankAipFile.path}, dir: ${bankAipFile.directory}"
+                echo "-Drepo.username=${nexusUser}"
+                sh "mvn deploy:deploy-file -DgeneratePom=true -DartifactId=${NEXUS_ARTIFACT} -Dversion=${NEXUS_VERSION}" +
+                        " -Dpackaging=zip -Dfile=${bankAipFile.path} -Durl=${nexusReleasesURL} -DgroupId=maven-public" +
+                        " -Drepo.usr=${nexusUser} -Drepo.pwd=${nexusPwd} -Dclassifier=distrib -e"
+            }
         }
-    }
 
 
 //    executeStage('Docker build', branch, stageResult) {
 //        // Загрузка сборка Докер Образ
 //        sh 'docker build . -t bankapi'
 //     }
-
+    }
 }
-
 
 
 def executeStage(stageName, branch, stageResult, Closure task) {

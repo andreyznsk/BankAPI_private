6a2328e311d9019aea768557301d8da6e0580c7f
andreyz <andreyznsk@gmail.com>
2022-02-24 10:05:07 +0700

    Add new module

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -77,7 +77,7 @@ node('ubuntu') {
         executeStage('upload to NEXUS', branch, stageResult) {
             // Загрузка билда в нексус
             withCredentials([usernamePassword(credentialsId: "admin", usernameVariable: "nexusUser", passwordVariable: "nexusPwd")]) {
-                def bankAipFile = findFiles(glob: 'BankAPIMain/bankAPI.zip')[0]
+                def bankAipFile = findFiles(glob: 'Service/BankAPIMain/bankAPI.zip')[0]
                 echo "Deploy NEXUS_VERSION: ${NEXUS_VERSION}"
                 echo "bankAipFile {name: ${bankAipFile.name}, path: ${bankAipFile.path}, dir: ${bankAipFile.directory}"
                 sh "git checkout BankApi-${NEXUS_VERSION}"
